d038ae5d6da4810b02793b20aae41affc3d815c6
getjyotikumari <46323070+getjyotikumari@users.noreply.github.com>
2023-10-02 13:51:59 +0530

    Delete hirehub-master directory

--- hirehub-master/routes/home.js
+++
@@ -1,8 +0,0 @@
-const express       = require('express'),
-      homeC 	  = require("../controllers/home")
-      isAuth        = require('../controllers/authMiddlewares').isAuth,
-      router        = express.Router(),
-
-router.get("/home", isAuth, homeC.homeGet);
-
-module.exports = router;
